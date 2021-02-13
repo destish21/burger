7eca1a87d395049103be63789d2bf11f0e1bd556
desta mulu <destish21@yahoo.com>
2021-02-13 15:51:13 -0600

    Updated orm.js querys

--- config/orm.js
+++ config/orm.js
@@ -1,37 +1,46 @@
 // Import MySQL connection.
 const connection = require('../config/connection.js');
-
 // Helper function for SQL syntax.
-const printQuestionMarks = (num) => {
+const printQuestionMarks = num => {
     var arr = []
     for (var i = 0; i < num; i++) {
         arr.push("?")
     }
     return arr.toString();
-},
+}
 
-// Helper function for SQL syntax.
-const objToSql = (ob) => {
+const objToSql = ob => {
+    console.log('objToSql')
+    
     var arr = [];
     for (var key in ob) {
-        if (Object.hasOwnProperty.call(ob, key)) {
-            arr.push(key + "=" + ob[key]);
-        }
+        arr.push(key + "=" + ob[key]);
     }
     return arr.toString();
-},
+}
+// // Helper function for SQL syntax.
+//  const objToSql = (ob) => {
+//     var arr = [];
+//     for (var key in ob) {
+//         if (Object.hasOwnProperty.call(ob, key)) {
+//             arr.push(key + "=" + ob[key]);
+//         }
+//     }
+//     return arr.toString();
+// },
 
 // Object for all our SQL statement functions.
 var orm = {
     selectAll: (tableInput, cb) => {
-        connection.query = "SELECT * FROM" + tableInput + ";", (err, result) => {
-            if (err) throw err;
+        connection.query("SELECT * FROM " + tableInput + ";", (err, result) => {
+            if (err) throw err; 
+            console.log('select * query' + result)
             cb(result);
-        };
+        });
     },
 
     insertOne: (table, columns, values, cb) => {
-        var query = "INSERT INTO" + table + " (" + columns.toString() + ") VALUES (" + printQuestionMarks(values.length) + ")";
+        var query = " INSERT INTO " + table + " (" + columns.toString() + ") VALUES (" + printQuestionMarks(values.length) + ")";
         connection.query(query, values, (err, result) => {
             if (err) throw err;
             cb(result);
@@ -39,12 +48,19 @@ var orm = {
     },
     // updateOne
     updateOne: ((table, objColVal, condition, cb) => {
-        var query = "UPDATE" + table + "SET" + objToSql(objColVal) + "WHERE" + condition;
+        var query = " UPDATE " + table + " SET " + objToSql(objColVal) + " WHERE " + condition;
         connection.query(query, (err, result) => {
             if (err) throw err;
             cb(result);
         });
     }),
+    delete:(table,condition, cb) => {
+        connection.query("DELELTE FROM ?? burgers WHERE ?;", [table, condition], (err, result) => {
+            if(err)throw err
+            cb(result)
+        })
+    }
+    
 };
 
 // Export the orm object for the model (burger.js).
