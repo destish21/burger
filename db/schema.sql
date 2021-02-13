d7bc9be1fc233d918e1da69550c387492892fe66
desta mulu <destish21@yahoo.com>
2021-02-13 15:56:37 -0600

    Updated db/seeds.sql

--- db/schema.sql
+++ db/schema.sql
@@ -2,8 +2,8 @@ DROP DATABASE IF EXISTS burgers_db;
 CREATE DATABASE burgers_db;
 USE burgers_db;
 DROP TABLE  IF EXISTS burgers;
-CREATE TABLE burgers;
-id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE burgers(
+id int NOT NULL AUTO_INCREMENT,
 burger_name VARCHAR(255) NOT NULL,
 devoured BOOL DEFAULT false,
-PRIMARY KEY (id)
+PRIMARY KEY (id));
